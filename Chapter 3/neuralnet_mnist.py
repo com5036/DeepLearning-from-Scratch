52a3ae4f391286fbfedc7d88da11f85ad44af2f0
com5036 <qkrwl5036@naver.com>
2021-04-25 23:33:13 +0900

    apply batch

--- Chapter 3/neuralnet_mnist.py
+++ Chapter 3/neuralnet_mnist.py
@@ -37,14 +37,16 @@ def predict(network, x):
 def main():
     x, t = get_data()
     network = init_network()
-    accuracy_cnt = 0
-    for i in range(len(x)):
-        y = predict(network, x[i])
-        p = np.argmax(y)
-        if p == t[i]:
-            accuracy_cnt += 1
 
-    print("Accuracy : " + str(float(accuracy_cnt)/len(x)))
+    batch_size = 100
+    accuracy_cnt = 0
+    for i in range(0, len(x), batch_size):
+        x_batch = x[i:i + batch_size]
+        y_batch = predict(network, x_batch)
+        p = np.argmax(y_batch, axis=1) # 추정되는 수
+        accuracy_cnt += np.sum(p == t[i:i + batch_size])
+
+    print("Accuracy : " + str(float(accuracy_cnt) / len(x)))
 
 
 main()
