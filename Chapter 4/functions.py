62033197517661898d72d46b62c12f7c988a9aa2
com5036 <qkrwl5036@naver.com>
2021-05-13 13:40:01 +0900

    gradient_desent_graph

--- Chapter 4/functions.py
+++ Chapter 4/functions.py
@@ -35,9 +35,12 @@ def numerical_gradient(f, x):
 
 def gradient_descent(f, init_x, lr=0.01, step_num=100):
     x = init_x
+    x_history = []
 
     for i in range(step_num):
+        x_history.append(x.copy())
+
         grad = numerical_gradient(f, x)
         x -= grad * lr
 
-    return x
+    return x, np.array(x_history)
---
+++ Chapter 4/gradient_desent_ex.py
@@ -0,0 +1,18 @@
+from functions import gradient_descent
+import numpy as np
+import matplotlib.pylab as plt
+
+
+def function_2(x):
+    return x[0] ** 2 + x[1] ** 2
+
+
+init_x = np.array([-3.0, 4.0])
+
+x, x_history = gradient_descent(function_2, init_x, lr=0.1, step_num=20)
+
+plt.plot([-5, 5], [0, 0], 'b')
+plt.plot([0, 0], [-5, 5], 'b')
+plt.plot((x_history[:, 0]), x_history[:, 1], 'o')
+
+plt.show()
